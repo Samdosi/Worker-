7c9eed915a81c807d659643a3e4658fda98308a2
Mohammad Ali Dosi <87053736+Samdosi@users.noreply.github.com>
2022-04-10 22:08:22 -0400

    Add files via upload

---
+++ Worker.java
@@ -0,0 +1,73 @@
+/* Mohammad Ali Dosi
+ * Dr. Steinberg
+ * COP3330 Spring 2022
+ * Programming Assignment 4
+ */
+public class Worker {
+    protected String name;
+    protected int hours;
+    public Worker(String name, int hours)
+    {
+        System.out.println("Worker(String name, int hours) has been invoked...");
+        this.name = name;
+        this.hours = hours;
+    }
+    public final void whoAreYou(){
+        System.out.println("My name is "+ name + " and I work " + hours + " hours a week.");
+
+    }
+    public void work(){
+        System.out.println("I am " + name  + " and I am doing my duty as the general worker for "+ hours + " hours a week.");
+    }
+    public final void updateHours(int hours){
+        System.out.println("Updating hours.");
+        this.hours = hours;
+        System.out.println("Updating hours is now complete.");
+    }
+}
+class Teacher extends Worker{
+    private int numstudents;
+    public Teacher(String name, int hours, int numstudents){
+        super(name, hours);
+        System.out.println("Teacher(String name, int hours, int numstudents) has been invoked...");
+        this.numstudents = numstudents;
+    }
+    public void work(){
+        System.out.println("I am " + name + " and I am doing my duty as the teacher.");
+        System.out.println("I have " + numstudents + " students and work " + hours + " hours a week.");
+    }
+    public void grade(){
+        System.out.println("Grading papers. The class did very well.");
+    }
+}
+class Doctor extends Worker{
+    private String type;
+    public Doctor(String name, int hours, String type){
+        super(name, hours);
+        System.out.println("Doctor(String name, int hours, String type) has been invoked...");
+        this.type = type;
+    }
+    public void work(){
+        System.out.println("I am " + name + " and I am doing my duty as the doctor.");
+        System.out.println("I am a "+ type + " and work " + hours + " hours a week.");
+    }
+    public void checkUp(){
+        System.out.println("Checking on patient.");
+    }
+}
+class CampCounselor extends Worker{
+    private double rate;
+    public CampCounselor(String name, int hours, double rate){
+        super(name, hours);
+        System.out.println("CampCounselor(String name, int hours, double rate) has been invoked...");
+        this.rate = rate;
+    }
+    public void work(){
+        System.out.println("I am " + name + " and I am doing my duty as the camp counselor.");
+        System.out.println("I am a camp counselor and work " + hours + " hours a week for " + rate + " per hour.");
+    }
+    public void activity(){
+        System.out.println("Let's race across the pool!");
+    }
+}
+
---
+++ WorkerRunner.java
@@ -0,0 +1,61 @@
+/* Name:
+ * Dr. Steinberg
+ * COP3330 Spring 2022
+ * Programming Assignment 4
+ */
+
+
+
+
+class WorkerRunner
+{
+	
+	public static void main(String [] args)
+	{
+		
+		//testing parent class
+		System.out.println("Begin Testing the Worker Class.");
+		
+		Worker worker1 = new Worker("John", 35);
+		worker1.whoAreYou();
+		worker1.work();
+		
+		System.out.println("*************************************************************************");
+		//testing child class
+		System.out.println("Begin Testing the Teacher Class.");
+		
+		Teacher worker2 = new Teacher("Hannah", 40, 35);
+		worker2.whoAreYou();
+		worker2.work();
+		worker2.grade();
+		
+		System.out.println("*************************************************************************");
+		//testing child class
+		System.out.println("Begin Testing the Doctor Class.");
+		
+		Doctor worker3 = new Doctor("Jane", 40, "Gastroenterologist");
+		worker3.whoAreYou();
+		worker3.work();
+		worker3.checkUp();
+		
+		System.out.println("*************************************************************************");
+		//testing child class
+		System.out.println("Begin Testing the CampCounselor Class.");
+		
+		CampCounselor worker4 = new CampCounselor("Peter", 10, 13.52);
+		worker4.whoAreYou();
+		worker4.work();
+		worker4.activity();
+		
+		System.out.println("*************************************************************************");
+		worker1.updateHours(38);
+		worker4.updateHours(20);
+		
+		System.out.println("*************************************************************************");
+		worker1.whoAreYou();
+		worker4.whoAreYou();
+		
+		System.out.println("End of testing...");
+		
+	}
+}
